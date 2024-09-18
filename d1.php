e620633ef9a347d7db85c526b361991d5d37a643
Dhruv Sadrawala <115005984+Dhruv-Sadrawala@users.noreply.github.com>
2024-09-18 11:23:05 +0530

    d1.php

--- d1.php
+++ d1.php
@@ -1,34 +1,37 @@
 <?php
 
-// Connect to the database using mysqli
 $con = mysqli_connect('localhost', 'root', '', 'mydatabase');
 
-// Check the connection
+
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Query to fetch the student data
 $qr = "SELECT * FROM student WHERE enroll_no='2022016190'";
 
-// Execute the query
+
 $var = mysqli_query($con, $qr);
 
-// Check if the query was successful
 if ($var) {
-    // Loop through the results and display each row
+    echo "<h1>This is student table.</h1>";
+    echo "<table cellspacing=2 border=2 width=100%>
+        <tr>
+        <th width=33%>Enroll No.</th><th width=33%>Name</th><th width=33%>DOB</th>
+        </tr>";
+    
     while ($row = mysqli_fetch_row($var)) {
-        echo "<table cellspacing=5>
-        <tr><td>{$row[0]}{$row[1]}{$row[2]}</td>
-        <td>{$row[0]}{$row[1]}{$row[2]}</td>
-        <td>{$row[0]}{$row[1]}{$row[2]}</td></tr>
-        </table>";
+        
+        echo"<tr><td align=center width=33%>{$row[0]}</td><td align=center width=33%>{$row[1]}</td><td align=center width=33%>{$row[2]}</td></tr>";
+        
     }
+    echo"</table>";
 } else {
     echo "Error: " . mysqli_error($con);
 }
 
-// Close the database connection
 mysqli_close($con);
 
 ?>
+
+
+registration form student validation , css roll name course gender hobbies date time onclick on next page in formated view
